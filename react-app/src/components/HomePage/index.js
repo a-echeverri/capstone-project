9635274fc69c764f662c12268efefdcf30a7ca78
Andres <aecheverri43@gmail.com>
2021-10-28 16:31:35 -0700

    homepage styling

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -6,10 +6,17 @@ export default function HomePage() {
 	return (
 		<div className="homepage">
 			<div className="homepage-content">
-				<h1 className="app-name-container">Welcome to Cocktail Party</h1>
-				<h2 className="homepage-text">Whether you’re making cocktails at home, ordering drinks at a bar, or browsing shelves at the liquor store, we’ll teach you everything you'll need to know about the drinks you love. We’ve curated the most essential cocktail recipes and bottles so you can learn more about your favorite drinks, and maybe even discover a few new ones.  </h2>
+                <div className="app-name-div">
+				    <h1 className="app-name-container">Welcome to Cocktail Party</h1>
+                </div>
+                <div className="homepage-text-div">
+				    <h2 className="homepage-text">Whether you’re making cocktails at home, ordering drinks at a bar, or browsing shelves at the liquor store, we’ll teach you everything you'll need to know about the drinks you love. We’ve curated the most essential cocktail recipes and bottles so you can learn more about your favorite drinks, and maybe even discover a few new ones. </h2>
+                </div>
+			</div>
+			<div className="homepage-image-div">
+				{/* <img src="./Manhattan-Riffs.png" alt="cocktail party"/> */}
+			</div>
 				<p className="homepage-text2">(Splash Page Work In Progress)</p>
-			</div>	
 		</div>
 	);
 };
