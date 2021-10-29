bdde96b42434d7dfccb4f240df57481657f1846f
Andres <aecheverri43@gmail.com>
2021-10-29 16:35:55 -0700

    Ingredients/Drinks centered

--- react-app/src/components/IngredientCategoriesPage/index.js
+++ react-app/src/components/IngredientCategoriesPage/index.js
@@ -2,8 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getCategoriesThunk } from "../../store/ingredientCategories";
-// import './CategoryList.css'
-
+import './IngredientCategoriesPage.css'
 function IngredientCategoriesPage() {
   const dispatch = useDispatch()
     console.log('dispatched')
